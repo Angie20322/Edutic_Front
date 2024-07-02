b65b162e75653e6e3005319d16095ee995deca26
danytorres132 <dsgamboa@ucundinamarca.edu.co>
2024-07-01 19:01:59 -0500

    Ajuste enlace estadísticas

--- src/components/Home/Paginas/Estadisticas/Estadisticas.jsx
+++ src/components/Home/Paginas/Estadisticas/Estadisticas.jsx
@@ -20,7 +20,7 @@ const Estadísticas = ({ open, setOpen, loginData }) => {
             <iframe
               className="w-[300px] md:w-[800px] h-[200px] md:h-[500px]"
               title="Estadísticas mostrar"
-              src="https://app.powerbi.com/view?r=eyJrIjoiOWU5MGM2ZTUtNDAwOS00ZDM5LThhODEtMjZmZjYxYTY0MTRjIiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9"
+              src="https://app.powerbi.com/reportEmbed?reportId=0086532e-3546-4ac2-8fbf-64b8e7110595&autoAuth=true&ctid=07da67a0-1f43-4e8c-977f-5f88b6470ee6"
               frameBorder="0"
               allowFullScreen="true"
             ></iframe>
